7d9fdfe9b0431027faceb116f2aa7ce98593efdb
alex.kot.1993@internet.ru <alex.kot.1993@internet.ru>
2021-11-08 11:27:29 +0300

    Финальные изменения.

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,29 @@
+from .pages.main_page import MainPage
+from .pages.loginpage import LoginPage
+from .pages.basket_page import BasketPage
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+	link = "http://selenium1py.pythonanywhere.com/"
+	page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+	page.open()                      # открываем страницу
+	page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+	
+def test_guest_should_see_login_link(browser):
+	link = "http://selenium1py.pythonanywhere.com/"
+	page = MainPage(browser, link)
+	page.open()
+	page.should_be_login_link()
+	
+def test_guest_should_see_URL(browser):
+	link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+	page = LoginPage(browser, link)
+	page.open()
+	page.should_be_login_url()
+	
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+	link = "http://selenium1py.pythonanywhere.com/"
+	page = BasketPage(browser, link)
+	page.open()
+	page.go_to_basket_page()
+	page.should_be_basket_empty()
+	page.should_be_basket_empty_message()
